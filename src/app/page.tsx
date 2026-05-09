59eb0122d7fb98856a4ee7ff97788f16dd831928
DeepGandhi07 <149680753+DGStacknyu@users.noreply.github.com>
2026-05-09 13:52:33 +0200

    Theme and Styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-import { Button } from "@/components/ui/button";
-import { authClient } from "@/lib/auth-client";
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 
 const Page = async () => {
   await requireAuth();
   const data = await caller.getUsers();
+
   return (
-    <div className="min-h-screen min-w-screen flex flex-col  items-center justify-center gap-y-6">
-      Protected Page
+    <div className="min-h-screen min-w-screen flex flex-col  items-center justify-center gap-y-3">
+      <div>Protected Page</div>
       <div>{JSON.stringify(data)}</div>
-      {/* <Button onClick={() => authClient.signOut()}>Logout</Button> */}
     </div>
   );
 };

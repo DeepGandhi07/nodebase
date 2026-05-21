ec36719ed147ea30375e594f83535d637cf8db28
DeepGandhi07 <149680753+DGStacknyu@users.noreply.github.com>
2026-05-21 09:32:48 +0200

    Implement workflows pagination and search functionality; add pagination and search components, update workflows query with parameters

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,14 +1,22 @@
 import WorkflowsList, {
   WorkflowsContainer,
 } from "@/features/workflows/components/workflow";
+import { workflowsParamsLoader } from "@/features/workflows/server/params-loader";
 import { prefetchWorkflows } from "@/features/workflows/server/prefetch";
 import { requireAuth } from "@/lib/auth-utils";
 import { HydrateClient, prefetch } from "@/trpc/server";
+import { SearchParams } from "nuqs/server";
 import { Suspense } from "react";
 import { ErrorBoundary } from "react-error-boundary";
-const Page = async () => {
+type Props = {
+  searchParams: Promise<SearchParams>;
+};
+
+const Page = async ({ searchParams }: Props) => {
   await requireAuth();
-  prefetchWorkflows();
+
+  const params = await workflowsParamsLoader(searchParams);
+  prefetchWorkflows(params);
   return (
     <WorkflowsContainer>
       <HydrateClient>
